774a11e89555bff9a03a421169feaeee450e5e6a
ghritak <ghritakjyotikalita@gmail.com>
2024-07-26 22:46:21 +0530

    Initial Commit

---
+++ Xpense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xpense
+//
+//  Created by Ghritak Jyoti Kalita on 26/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xpense/XpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  XpenseApp.swift
+//  Xpense
+//
+//  Created by Ghritak Jyoti Kalita on 26/07/24.
+//
+
+import SwiftUI
+
+@main
+struct XpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
